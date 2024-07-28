b738d38311df3d56921ae50e658873df0e6878cf
Asad Khan <asad219@hotmail.com>
2024-07-28 20:36:10 +0400

    add comment in user router

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,10 +2,11 @@ const express = require('express');
 const router = express.Router();
 const authenticateKey = require('../middlewares/authApi');
 const { getAll, register, login, getUserById } = require('../controllers/userController');
-
+//user routes
 router.get("/getall", authenticateKey, getAll);
 router.get("/:id", getUserById);
 router.post("/register", register);
 router.post("/login", authenticateKey, login);
 
 module.exports = router;
+
