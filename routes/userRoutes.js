9cb9c5b6f480e258f72f553a78141c3113b7d1b6
Asad Ullah Khan <asad219@hotmail.com>
2024-08-08 15:53:16 +0400

    merge changes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,8 +4,8 @@ const authenticateKey = require('../middlewares/authApi');
 const validateToken = require('../middlewares/validateTokenHandler');
 const { getAll, register, login, getUserById } = require('../controllers/userController');
 
-router.get("/getall", authenticateKey, getAll);
-router.get("/:id", getUserById);
+router.get("/getall", validateToken, getAll);
+router.get("/:id", validateToken,getUserById);
 router.post("/register", register);
 router.post("/login", login);
 
